9b5a31691e5b90cb6e6e0725e4b8de6c25edbe27
chuotit <tiencong.com@gmail.com>
2017-06-24 23:56:33 +0700

    Lesson 26 Tạo form tìm kiếm

--- TeduShop.Web/app/components/product-categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product-categories/productCategoryListController.js
@@ -7,18 +7,25 @@
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
+        $scope.keyword = '';
         $scope.getProductCategories = getProductCategories;
-        
+
+        $scope.search = search;
+
+        function search() {
+            getProductCategories();
+        }
 
         function getProductCategories(page) {
             page = page || 0;
             var config = {
                 params: {
+                    keyword: $scope.keyword,
                     page: page,
                     pagesize: 3
                 }
             }
-
+            
             apiService.get('/api/productcategory/getall', config, function (result) {
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
